c535d7c7b2eb116047b9c6f1a307422de7b3bfe9
odolgavik <822376@gmail.com>
2021-11-15 00:25:52 +0500

    Add lesson4

--- src/main/yava/lesson4/lesson4.java
+++ src/main/yava/lesson4/lesson4.java
@@ -1,4 +1,9 @@
 package lesson4;
 
 public class lesson4 {
+    public static void main(String[] args) {
+       for (int corenkvadr = 9; corenkvadr <= 0; ++corenkvadr) {
+        System.out.println("Корень квадратный равен" + corenkvadr);
+    }
+}
 }
