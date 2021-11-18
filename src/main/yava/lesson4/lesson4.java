93921a01cf7322c84778874fb7fc1429a1af229d
odolgavik <822376@gmail.com>
2021-11-19 01:04:37 +0500

    Add 3 4 lesson

--- src/main/yava/lesson4/lesson4.java
+++ src/main/yava/lesson4/lesson4.java
@@ -1,9 +1,31 @@
 package lesson4;
 
+import java.util.Scanner;
+
 public class lesson4 {
+
     public static void main(String[] args) {
-       for (int corenkvadr = 9; corenkvadr <= 0; ++corenkvadr) {
-        System.out.println("Корень квадратный равен" + corenkvadr);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите число для расчета целочисленного квадратного корня: ");
+        int i= scanner.nextInt();
+
+        findSqrtRoot(i);
     }
-}
+
+    static void findSqrtRoot(int i) {
+        if (i < 0) {
+            System.out.println("Корень из отрицательного числа не существует");
+            return;
+        }
+        int sqrt = i;
+        while (i != sqrt * sqrt) {
+            sqrt--;
+            if (sqrt == 0) {
+                System.out.println("Не найдено целочисленных корней");
+                return;
+            }
+        }
+
+        System.out.println("Корень числа " + i + " равен +-" + sqrt);
+    }
 }
